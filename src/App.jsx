2f342d8f6cc5db3c1b78f84d8893ab0f13112f61
lazyshivam <shivamgoswami.ss.pp@gmail.com>
2024-04-03 13:20:36 +0530

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-center'>
+      <h1 className='text-red-900'>Hello there..</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
